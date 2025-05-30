33c850c847508de3793b6b0c6072c8c01af575cb
Krish Singhal <krishsinghal006@gmail.com>
2025-05-30 20:41:09 +0000

    Remove download folder

--- download/src/lib/firebase.ts
+++
@@ -1,7 +0,0 @@
-// This file previously contained Firebase initialization code.
-// It has been emptied to revert to a mock data setup as Firebase
-// initialization was causing errors.
-
-// If you intend to use Firebase in the future, you would re-add
-// your Firebase app initialization code here.
-// console.log("Firebase module loaded, but is intentionally not configured for mock data usage.");
